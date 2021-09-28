82b6f8066bc649f4e9b7e223df5be4da8b707808
chetto <dylan.stanley.clark@gmail.com>
2021-09-28 19:04:29 -0400

    restored files

---
+++ README.md
@@ -0,0 +1,4 @@
+# LOP_role_table
+#### Project to process the Library of Parliament role table to a structured shape.
+
+This project has been created by the Data Science Platform.
---
+++ src/main.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from collections import Counter
+from datetime import timedelta, date,datetime
+import numpy as np
+import json
+import argparse
+import os
+def create_argument_parser():
+    """
+    Function to add command line arguments at run time
+    """
+    parser  = argparse.ArgumentParser(description = 'head-tail: Grabs N head or tail rows from a text file.')
+    parser.add_argument('--input-file', nargs = '?', required = False, help = 'The path of the input file.')
+    return parser
+
+
+if __name__ == "__main__":
+    # add command line arguments to send
+    parser  = argparse.ArgumentParser(description = 'Script to proccess LOP role table')
+    args = parser.parse_args()
+    aws_key = os.environ['aws_key']
+    #print(args.input_file)
+    print(aws_key,"Some other stuff too and more")
